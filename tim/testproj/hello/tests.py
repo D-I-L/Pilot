daf92ddbc29042e8d74feded333f9aef9c5b43e4
tcarver <tjc29@duffield>
2015-02-10 11:41:18 +0000

    add comments

--- tim/testproj/hello/tests.py
+++ tim/testproj/hello/tests.py
@@ -3,11 +3,17 @@ from django.test import TestCase
 
 class HelloTestCase(TestCase):
 
+    '''
+    Test the page status and a variable (greet) in the response
+    '''
     def test_page(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.context['greet'], 'hello')
 
+    '''
+    Test the status of a fictional page
+    '''
     def test_page_not_found(self):
         resp = self.client.get('/xxx/')
         self.assertEqual(resp.status_code, 404)
