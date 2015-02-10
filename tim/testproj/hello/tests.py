daa634d35b1cd9862ebb1d4a7244e1ed5083cad0
tcarver <tjc29@duffield>
2015-02-10 11:27:31 +0000

    simplify page context test

--- tim/testproj/hello/tests.py
+++ tim/testproj/hello/tests.py
@@ -6,9 +6,7 @@ class HelloTestCase(TestCase):
     def test_page(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
-
-        if resp.context is not None:  # if none it is cached
-            self.assertEqual(resp.context['greet'], 'hello')
+        self.assertEqual(resp.context['greet'], 'hello')
 
     def test_page_not_found(self):
         resp = self.client.get('/xxx/')
