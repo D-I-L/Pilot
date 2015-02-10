ab4f4fcb97ba7bdea176b8d648e6e7e298f85837
tcarver <tjc29@duffield>
2015-02-10 11:46:48 +0000

    fix for pep8

--- tim/testproj/testproj/urls.py
+++ tim/testproj/testproj/urls.py
@@ -1,11 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'testproj.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    #url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('hello.urls')),
-)
-
+                       url(r'^$', include('hello.urls')),
+                       )
